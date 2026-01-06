e4740de552b4d1c6a78ea2cdc15f1a31dc8ee266
jltlam <jlam011235@gmail.com>
2026-01-05 22:33:00 -0500

    updated resume, about

--- src/components/section/aboutSection/AboutSection.jsx
+++ src/components/section/aboutSection/AboutSection.jsx
@@ -9,10 +9,10 @@ export default function AboutSection({title}) {
         <h1 className='sectionTitle'>{title}</h1>
         <h1>Juliet Lam</h1>
         <p>
-            Hi! I'm Juliet Lam, a Senior Software Engineering student at Rochester
-            Institute of Technology. I enjoy problem-solving and finding solutions.
-            I have experience in full-stack web development from internships and coursework,
-            and an interest in animation (2D and 3D)!
+            Hi! I'm Juliet Lam, a recent graduate in Software Engineering at Rochester
+            Institute of Technology. I enjoy problem-solving and learning new topics.
+            I have experience in full-stack development from internships and coursework,
+            and animation is my hobby! (Mostly 2D, but 3D is cool, too.)
         </p>
         <p>
             Completed coursework includes Database and Data Modeling, 
@@ -20,10 +20,10 @@ export default function AboutSection({title}) {
             Software Design for Computing Systems, Computer Graphics, Client and Server Programming.
             Currently taking Networking, Introduction to AI, and finding those very interesting.
         </p>
-        <p><b>
-            If you find any of this interesting (or employable &#x1f441;&#x1f441;),
+        <p>
+            If you find any of this interesting &#x1f441;&#x1f441;,
             feel free to reach out!
-            I am seeking employment after June 2025. (Graduation: May 2025)</b>
+            {/* I am seeking employment after June 2025. */}
         </p>
         <p>
             For more contact or more information via links, check the Contacts section.
